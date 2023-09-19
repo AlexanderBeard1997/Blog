a5ac3b63999446b06bc1f5668dee3e61cc1446be
jamezz6 <92213021+jamezz6@users.noreply.github.com>
2023-09-19 14:09:22 +0100

    Update items2.js

---
+++ backend/routes/items2.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router() // create a router
+const itemsController = require('../controllers/items')
+
+// routes to controllers
+// the '/items' route will run the getTodos controller
+// which will fetch the data from your database
+router.get('/', itemsController.getUser)
+router.get('/:id', itemsController.getUser)
+router.post('/item', itemsController.createUser)
+router.patch('/:id', itemsController.editUser)
+router.delete('/:id', itemsController.deleteUser)
+module.exports = router
